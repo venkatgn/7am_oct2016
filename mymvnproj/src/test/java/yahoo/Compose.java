a10cc86ae8cc5952e65acdfb5b76e2586e8a6e93
venkatgn <venkatadrin.g@gmail.com>
2016-11-01 09:43:40 +0530

    kdsflsdf

--- mymvnproj/src/test/java/yahoo/Compose.java
+++ mymvnproj/src/test/java/yahoo/Compose.java
@@ -23,6 +23,7 @@ public class Compose
 	}
   public void sendmail() throws Exception
   {
+	  System.out.println("sending a new mail");
 	  driver.findElement(By.xpath("//input[@class='composeicon']")).click();
 	  Thread.sleep(3000);
 	  driver.findElement(By.id("to")).sendKeys("abcd@gmail.com");
